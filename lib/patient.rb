675472faecd59a9387b58e32005ff7d8281bf36e
Chance Smith <chancesmith97@hotmail.com>
2019-03-30 23:11:11 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,22 +1,24 @@
 class Patient
-  attr_accessor :name 
-  
+
+  attr_reader :name, :appointment, :doctor
+
   def initialize(name)
-    @name =name 
-    @appointments= []
+    @name = name
   end
-  
-  def add_appointment(appointment)
-    @appointments << appointment
-    appointment.patient = self 
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
   end
-  
+
   def appointments
-    @appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
   end
-  
-  def doctors 
-    self.appointments.collect {|app| app.doctor}
+
+  def doctors
+    Appointment.all.map do |appointment|
+      appointment.doctor
+    end
   end
-  
 end
